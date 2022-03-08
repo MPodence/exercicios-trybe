203711e0c8e2dda7a8984346f89299de3fe8ed96
Podence <podence23@gmail.com>
2022-03-08 17:15:48 -0300

    exercicio 1

---
+++ Fundamentos/bloco04-javascript/exercicios-javascript.js
@@ -0,0 +1,22 @@
+//exercicio 1
+const valorA = 222;
+const valorB = 23;
+let resultado = null;
+
+resultado = valorA + valorB;
+console.log(valorA, '+', valorB, '=', resultado);
+
+resultado = valorA - valorB;
+console.log(valorA, '-', valorB, '=', resultado);
+
+resultado = valorA * valorB;
+console.log(valorA, '*', valorB, '=', resultado);
+
+resultado = valorA / valorB;
+console.log(valorA, '/', valorB, '=', resultado);
+
+resultado = valorA % valorB;
+console.log(valorA, '%', valorB, '=', resultado, 'é o resto da divisão')
+
+
+
